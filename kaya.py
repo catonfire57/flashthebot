088e6eb7f56b4fec0914f9d4d913f8ca396429c4
Saksham Jain <76650664+catonfire57@users.noreply.github.com>
2024-12-29 14:36:05 +0530

    New Update, Now kaya doesn't need any external program to startup

--- kaya.py
+++ kaya.py
@@ -1,4 +1,4 @@
-import pyttsx3
+import pyttsx3 
 import speech_recognition as sr
 import datetime
 import os
@@ -14,13 +14,15 @@ import winsound
 import pyjokes
 import threading
 import pyautogui
+from pywikihow import search_wikihow
+import psutil
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 
 # print(voices[1].id)
-engine.setProperty('rate', 190)
-engine.setProperty('voices', voices[0].id)
+engine.setProperty('rate', 200)
+engine.setProperty('voices', voices[1].id)
 
 # text to speech
 
@@ -34,6 +36,7 @@ def takecommand():
     with sr.Microphone() as source:
         print("LISTENING...")
         r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
     
     try:
@@ -42,7 +45,6 @@ def takecommand():
         print(f"user said: {query}")
 
     except Exception as e:
-        print("ahan?")
         return ""
     return query
 
@@ -63,6 +65,18 @@ def wish():
 
     #thread = threading.Thread(target=reminder_thread)
     #thread.start()
+
+
+def batterycheck_thread():
+    battery = psutil.sensors_battery()
+    percent = battery.percent
+    if percent<=64 and percent>=63:
+        speak("sir, the battery running low, pls connect the charger")
+        time.sleep(300)
+    time.sleep(300)
+thread = threading.Thread(target=batterycheck_thread)
+thread.start()
+
 
 def typewrite():
     print("INITIATING TYPEWRITE")
@@ -204,11 +218,12 @@ sites = [
 ]
 
 
-if __name__ == "__main__":
+def kayathebot():
     wish()
     while True:
 
         query = takecommand().lower()
+        assisted = False
 
 # ALL TASKS LOGIC
 
@@ -216,19 +231,23 @@ if __name__ == "__main__":
             if f"{site[0]}".lower() in query.lower():
                 speak("Sure, here you go")
                 wb.open(site[1])
+                assisted = True
 
 
         if "notepad" in query:
             speak("sure")
             npath = "C:\\Program Files\\WindowsApps\\Microsoft.WindowsNotepad_11.2410.21.0_x64__8wekyb3d8bbwe\\Notepad\\Notepad.exe"
             os.startfile(npath)
+            assisted = True
 
         if any(x in query for x in ["start typing", "type what I say"]):
             typewrite()
+            assisted = True
         
         if "cmd" in query or "command prompt" in query:
             speak("sure, lesgo catto")
             os.system("start cmd")
+            assisted = True
 
         #if "webcam" in query:
             #cap = cv2.VideoCapture(0)
@@ -244,9 +263,11 @@ if __name__ == "__main__":
         if "ip" in query:
             ip = get('https://api.ipify.org').text
             speak(ip)
+            assisted = True
 
-        if any(x in query for x in ["hello kaya", "kaya you up", "alive", "wake up"]):
+        if any(x in query for x in ["hello kaya", "kaya you up", "alive", "wake up", "hello kya"]):
             speak("hello sir, I'm up and working fine")
+            assisted = True
 
         
 
@@ -254,11 +275,14 @@ if __name__ == "__main__":
             speak("sure, opening whatsapp")
             npath = "C:\\Users\\saksh\\Desktop\\Shortcuts ( dont delete )\\WhatsApp.lnk"
             os.startfile(npath)
+            assisted = True
        
         if "settings" in query:
             pyautogui.hotkey("win", "i")
+            assisted = True
 
         if "send message to" in query:
+            assisted = True
             query = query.replace("send message to", "")
             person = query
             speak("what msg to send sir?")
@@ -282,60 +306,69 @@ if __name__ == "__main__":
             else:
                 print("speak again")
                 continue
-        else:
-            speak("I can't assist with that yet, but I'm learning!")
-            continue
 
 
         if "chat gpt" in query:
             speak("kk")
             npath = "C:\\Users\\saksh\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Chrome Apps\\ChatGPT.lnk"
             os.startfile(npath)
+            assisted = True
 
         if "open genshin" in query or "open genshin impact" in query:
             npath = "C:\\Program Files\\Epic Games\\GenshinImpact\\games\\Genshin Impact game\\GenshinImpact.exe"
             os.startfile(npath)
+            assisted = True
 
-        if "thank" in query:
+        if any(x in query for x in ["thankyou", "thank", "crazy", "amazing"]):
             speak("my pleasure master")
+            speak("anything else I can help with?")
+            assisted = True
 
 
         if "search on google" in query or "search google for" in query:
             speak("Sure, searching google for this?")
             query = query.replace("search", "").replace("google", "").replace("for", "").replace("on", "")
             wb.open(f"https://www.google.com/search?q={query}")
+            assisted = True
 
         if "anime"in query:
             speak("here you go weeb")
             npath = "C:\\Users\\saksh\\Desktop\\Shortcuts ( dont delete )\\hianime.to.lnk"
             os.startfile(npath)
+            assisted = True
 
-        if "song" in query:
-            speak("sure, what song to play?, maestro")
-            song = takecommand().lower()
-            kit.playonyt(song)
+        if any(x in query for x in ["play song", "play"]):
+            query = query.replace("song", "").replace("play", "").replace("the", "").replace("kaya", "")
+            speak(f"playing {query}")
+            kit.playonyt(query)
+            assisted= True
 
         
-        if any(x in query for x in ["sleep", "exit", "rest", "quit"]):
+        if any(x in query for x in ["sleep", "exit", "rest", "quit", "shut down", "shutdown", "standby"]):
             speak("goodbye sir, see you soon")
-            winsound.Beep(900, 500)
-            sys.exit()
+            speak("standby")
+            assisted = True
+            break
         
         if "time" in query:
             strfTime = datetime.datetime.now().strftime("%H:%M")
             speak(strfTime)
+            assisted = True
 
         if "date" in query:
             now = datetime.datetime.now()
             date_only = now.date()
             speak(date_only)
+            assisted = True
 
         if any(x in query for x in ["parrot", "vm", "virtual machine"]):
+            assisted = True
             speak("opening the parrot v m")
             npath = "C:\\Users\\saksh\\Documents\\Virtual Machines\\parrotVM\\parrotVM.vmx"
             os.startfile(npath)
 
         if "joke" in query:
+            assisted = True
             speak(pyjokes.get_joke())
 
         #if "remind me" in query:
@@ -346,7 +379,8 @@ if __name__ == "__main__":
             #seconds = minute * 60
             #set_reminder(reminder, seconds)
 
-        if "switch window" in query or "change window" in query:            
+        if any(x in query for x in ["change my window", "change window", "change this window"]):
+            assisted = True         
             pyautogui.keyDown("alt")
             pyautogui.press("tab")
             speak("this?")
@@ -359,8 +393,50 @@ if __name__ == "__main__":
                     pyautogui.keyUp("alt")
                     break
 
+        if any(x in query for x in ["switch my window", "switch window", "switch this window"]):
+            pyautogui.hotkey("alt", "tab")
+            assisted = True
+
         if "screen navigation" in query:
-            screen_navigation() 
+            assisted = True
+            screen_navigation()
 
         if any(x in query for x in ["close this window", "close window", "close this"]):
+            assisted = True
             pyautogui.hotkey("alt", "f4")
+
+        #if not assisted:
+            #speak("Sorry, I can't assist you with that, but I'm still learning")
+
+        if "stop" in query:
+            assisted = True
+            speak("ok")
+
+        if "how to" in query:
+            assisted = True
+            query = query.replace("how to", "").replace("kaya", "").replace("tell me", "")
+            max_results = 1
+            how_to = search_wikihow(query, max_results)
+            assert len(how_to) == 1
+            how_to[0].print()
+            speak(how_to[0].summary)
+        
+        if any(x in query for x in ["battery"]):
+            assisted = True
+            battery = psutil.sensors_battery()
+            percent = battery.percent
+            if percent<20:
+                speak(f"this system has only {percent} percent battery left, I sense the need of connecting the charger")
+            else:
+                speak(f"we have {percent} percent battery right now")
+
+
+if __name__ == "__main__":
+    while True:
+        permission = takecommand().lower()
+        if any(x in permission for x in ["wake up", "breakup"]):
+            kayathebot()
+        if any(x in permission for x in ["terminate", "shut down", "shutdown", "kill system"]):
+            speak("kaya has been terminated for now")
+            winsound.Beep(900, 500)
+            sys.exit()
